b01614c6f9437b1a737e8f8bffd2c1b9fb69d37b
Alexey <a99lex@yandex.ru>
2022-10-31 16:32:46 +0500

    add Footer component

---
+++ src/component/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <button>Все задачи</button>
+      <button>Активные</button>
+      <button>Завершенные</button>
+    </div>
+  );
+};
+
+export default Footer;
